8a12be3416b5ce171d8b6565d0cff181d5112e70
a-leblond <alexis.l@outlook.com>
2019-01-16 16:19:39 +0100

    Add new opcodes

--- lib/cpu.dart
+++ lib/cpu.dart
@@ -36,6 +36,18 @@ class CPU {
       case 0x0e: unimplementedInstruction(); break;
       case 0x0f: unimplementedInstruction(); break;
       /* */
+      case 0x2f: //CMA (not)
+        state.a = ~state.a;
+        break;
+      case 0xe6: //ANI byte
+        int x = state.a & state.memory[state.pc+1];
+        state.cc.z = (x == 0) ? 1 : 0;
+        state.cc.s = (0x80 == (x & 0x80)) ? 1 : 0;
+        state.cc.p = parity(x, 8);
+        state.cc.cy = 0;
+        state.a = x;
+        state.pc++;
+        break;
       case 0x80: //ADD B
         int answer = state.a + state.b;
         state.cc.z = ((answer & 0xff) == 0) ? 1 : 0;
@@ -52,13 +64,30 @@ class CPU {
         state.cc.p = parity(answer & 0xff,8);
         state.a = answer & 0xff;
         break;
-      case 0xC6: //ADI byte
+      case 0xc2: //JNZ address
+        state.pc = (0 == state.cc.z) ? (state.memory[state.pc+2] << 8) | state.memory[state.pc+1] : state.pc + 2;
+        break;
+      case 0xc3: //JMP address
+        state.pc = (state.memory[state.pc+2] << 8) | state.memory[state.pc+1];
+        break;
+      case 0xc6: //ADI byte
         int answer = state.a + state.memory[state.pc+1];
         state.cc.z = ((answer & 0xff) == 0) ? 1 : 0;
         state.cc.s = ((answer & 0x80) != 0) ? 1 : 0;
         state.cc.cy = (answer > 0xff) ? 1 : 0;
         state.cc.p = parity(answer & 0xff,8);
         state.a = answer & 0xff;
+        break;
+      case 0xc9:
+        state.pc = state.memory[state.sp] | (state.memory[state.sp+1] << 8);
+        state.sp += 2;
+        break;
+      case 0xcd:
+        int ret = state.pc+2;
+        state.memory[state.sp-1] = (ret >> 8) & 0xff;
+        state.memory[state.sp-2] = (ret & 0xff);
+        state.sp = state.sp - 2;
+        state.pc = (state.memory[state.pc+2] << 8) | state.memory[state.pc+1];
         break;
       case 0x86: //ADD M
         int offset = (state.h<<8) | (state.l);
